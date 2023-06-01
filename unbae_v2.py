edfac36f9635536e4d975c3a7e3b8f5450b14646
csl <734644395@qq.com>
2023-06-02 00:06:25 +0800

    unbae_v2.py

--- unbae_v2.py
+++ unbae_v2.py
@@ -72,7 +72,7 @@ def main(args):
             if len(choice) != 0:
                 add_pos.append(choice[0]+(index,))
         add_pos.sort(reverse=True)
-        add_pos = add_pos[:min(len(add_pos),1)]
+        add_pos = add_pos[:min(len(add_pos),2)]
         add_pos.sort(key=lambda x:(x[2]),reverse=True)
         inserted_ids = input_ids.copy()
         for tp in add_pos:
