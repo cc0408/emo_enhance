4635fdd259cbb9c66a69085f3c6aee246444490e
csl <734644395@qq.com>
2023-06-04 16:39:41 +0800

    roberta.py

--- roberta.py
+++ roberta.py
@@ -163,7 +163,7 @@ train_masks = torch.tensor(train_masks)
 validation_masks = torch.tensor(validation_masks)
 
 # Select a batch size for training. For fine-tuning BERT on a specific task, the authors recommend a batch size of 16 or 32
-batch_size = 32
+batch_size = 100
 
 # Create an iterator of our data with torch DataLoader. This helps save on memory during training because, unlike a for loop, 
 # with an iterator the entire dataset does not need to be loaded into memory
@@ -191,11 +191,11 @@ type(train_dataloader)
 model = RobertaForSequenceClassification.from_pretrained("roberta-base", num_labels=6).to(device)
 
 # Parameters:
-lr = 1e-7
+lr = 1e-6
 adam_epsilon = 1e-8
 
 # Number of training epochs (authors recommend between 2 and 4)
-epochs = 10
+epochs = 5
 
 num_warmup_steps = 0
 num_training_steps = len(train_dataloader)*epochs
