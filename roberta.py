a68b184cdaee249a447e249361fd661d76cd07a4
csl <734644395@qq.com>
2023-06-05 00:46:45 +0800

    roberta.py

--- roberta.py
+++ roberta.py
@@ -163,7 +163,7 @@ train_masks = torch.tensor(train_masks)
 validation_masks = torch.tensor(validation_masks)
 
 # Select a batch size for training. For fine-tuning BERT on a specific task, the authors recommend a batch size of 16 or 32
-batch_size = 100
+batch_size = 32
 
 # Create an iterator of our data with torch DataLoader. This helps save on memory during training because, unlike a for loop, 
 # with an iterator the entire dataset does not need to be loaded into memory
