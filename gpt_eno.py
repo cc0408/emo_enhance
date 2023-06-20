c2c3c4f170e521d15347698662fe2af839c6150e
csl <734644395@qq.com>
2023-06-20 20:13:40 +0800

    gpt_eno.py

--- gpt_eno.py
+++ gpt_eno.py
@@ -99,8 +99,10 @@ if __name__ == '__main__':
             n=1,
             best_of=1
         )
-        res.append([results['response']['choices'][0]['message']['content'].strip('\"')])
-        # if idx % 50 == 0:
+        ss = results['response']['choices'][0]['message']['content'].strip('"')
+        res.append([ss])
+        if idx % 30 == 0:
+            print(idx)
         print(idx)
     res = pd.DataFrame(res)
     res.to_csv(output_path, index=False, header=False)
