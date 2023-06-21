9b215f09ccd2dd905305b7189a8e8af546067a79
csl <734644395@qq.com>
2023-06-21 22:33:00 +0800

    g_

--- g_bert-base-uncased.py
+++ g_bert-base-uncased.py
@@ -195,13 +195,13 @@ lr = 2e-5
 adam_epsilon = 1e-8
 
 # Number of training epochs (authors recommend between 2 and 4)
-epochs = 5
+epochs = 3
 
 num_warmup_steps = 0
 num_training_steps = len(train_dataloader)*epochs
 
 ### In Transformers, optimizer and schedules are splitted and instantiated like this:
-optimizer = AdamW(model.parameters(), lr=lr,eps=adam_epsilon,correct_bias=False,weight_decay=0.01)  # To reproduce BertAdam specific behavior set correct_bias=False
+optimizer = AdamW(model.parameters(), lr=lr,eps=adam_epsilon,correct_bias=False)  # To reproduce BertAdam specific behavior set correct_bias=False
 scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=num_warmup_steps, num_training_steps=num_training_steps)  # PyTorch scheduler
 
 
