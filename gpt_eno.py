445f9ae62175b3667c8220f6f81963ee203c0deb
csl <734644395@qq.com>
2023-06-22 13:39:17 +0800

    gpt_eno.py

--- gpt_eno.py
+++ gpt_eno.py
@@ -93,7 +93,7 @@ if __name__ == '__main__':
         sentence = dataset['test'][idx]['sentence']
         label = dataset['test'][idx]['label']
         label = int2label[label]
-        messages = [{"role": "system", "content": "You are asked to edit the text by changing no more than 3 words so that the output sentence is very similar to original sentence."},
+        messages = [{"role": "system", "content": "You are asked to edit the text by changing no more than 3 words so that the two sentences are mostly the same. And the edit distance need to be less than 3."},
                     {"role": "user","content": f"The original sentence is :{sentence}. The sentence that has a stronger {label} emotion and maintains the same semantics by adding and replacing within three words is:"}]
         results = make_requests(
             engine="gpt-3.5-turbo-0613",
