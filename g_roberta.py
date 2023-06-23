c8c7aab7d63d534a8ffd7cade3ed7ef2c9a06f35
csl <734644395@qq.com>
2023-06-23 14:10:01 +0800

    eval_v2.py

--- g_roberta.py
+++ g_roberta.py
@@ -190,7 +190,7 @@ type(train_dataloader)
 model = AutoModelForSequenceClassification.from_pretrained("roberta-large", num_labels=6).to(device)
 
 # Parameters:
-lr = 2e-5
+lr = 1e-5
 adam_epsilon = 1e-8
 
 # Number of training epochs (authors recommend between 2 and 4)
