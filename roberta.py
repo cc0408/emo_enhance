52b6ed58a6ad2b5d122301b742ae51c2d2cb6cf5
csl <734644395@qq.com>
2023-06-23 16:42:30 +0800

    roberta.py

--- roberta.py
+++ roberta.py
@@ -15,7 +15,7 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 # For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
 
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = '1'
+os.environ["CUDA_VISIBLE_DEVICES"] = '2'
 for dirname, _, filenames in os.walk('/home/xuxi/emo_enhance/data'):
     for filename in filenames:
         print(os.path.join(dirname, filename))
@@ -188,20 +188,21 @@ type(train_dataloader)
 
 
 # Load RobertaForSequenceClassification, the pretrained BERT model with a single linear classification layer on top. 
-model = AutoModelForSequenceClassification.from_pretrained("roberta-base", num_labels=6).to(device)
+model = AutoModelForSequenceClassification.from_pretrained("roberta-large", num_labels=6).to(device)
 
 # Parameters:
 lr = 1e-5
 adam_epsilon = 1e-8
 
 # Number of training epochs (authors recommend between 2 and 4)
-epochs = 10
+epochs = 15
 
 num_warmup_steps = len(train_dataloader)*2
 num_training_steps = len(train_dataloader)*epochs
 
 ### In Transformers, optimizer and schedules are splitted and instantiated like this:
-optimizer = AdamW(model.parameters(), lr=lr,eps=adam_epsilon,correct_bias=False)  # To reproduce BertAdam specific behavior set correct_bias=False
+from torch.optim import Adam
+optimizer = Adam(model.parameters(), lr=lr) # To reproduce BertAdam specific behavior set correct_bias=False
 scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=num_warmup_steps, num_training_steps=num_training_steps)  # PyTorch scheduler
 
 
